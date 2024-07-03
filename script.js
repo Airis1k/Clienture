c50edeb9b4bbdd6d5f6b858792f0b56d428d8a53
Airis1k <airingasstar@gmail.com>
2024-07-03 11:35:14 +0300

    added footer

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function main() {
+   addDateInHTML();
+}
+
+function addDateInHTML() {
+   var now = new Date();
+   var datetime = now.getFullYear();
+
+   document.getElementById("datetime").innerHTML = `Copyright &copy; ${datetime}. All rights reserved.`;
 }
 
 main();
